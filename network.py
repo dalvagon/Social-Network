67d01fb3d0d4ab09f1e3aba66ddcfc265cf62a2b
Leahu Vlad Iulius <leahu.vlad.iulius@gmail.com>
2023-04-19 10:34:55 +0300

    Document code

--- network.py
+++ network.py
@@ -2,15 +2,21 @@ from collections import deque
 
 
 class SocialNetwork:
+    """A social network is a collection of users and their friendships."""
+
     def __init__(self, frienships):
+        """
+        Build a social network from a list of friendships.
+        Parameters:
+            friendships: list
+                A list of tuples containing the names of two users that are friends.
+        """
         self.frienships = frienships
         self.users = set()
-        try:
-            self.buildNetwork()
-        except ValueError as e:
-            raise ValueError(e)
+        self.buildNetwork()
 
     def buildNetwork(self):
+        """Build the network from the list of friendships"""
         for user1, user2 in self.frienships:
             if user1 == user2:
                 raise ValueError("User cannot be friend with himself")
@@ -21,12 +27,25 @@ class SocialNetwork:
             user2.add_friend(user1)
 
     def user(self, name):
+        """Return the user with the given name if it exists, otherwise return None"""
         for user in self.users:
             if user.name == name:
                 return user
         return None
 
     def shortest_path(self, userA, userB):
+        """
+        Find the shortest path between two users in the network using breadth-first search.
+        Parameters:
+            userA: User
+                The first user's name.
+            userB: User
+                The second user's name.
+        Returns:
+            tuple
+                A tuple containing a list of the users in the shortest path and the length of the path.
+                If there is no path, the list should be None and the length should be -1.
+        """
         start = self.user(userA)
         end = self.user(userB)
 
@@ -40,18 +59,20 @@ class SocialNetwork:
         queue.append((start, 0))
         visited = set()
         visited.add(start)
-        prev = {start: None}
+        prev = {start: None}  # previous node in the path
 
         while queue:
             user, depth = queue.popleft()
-            if user == end:
+            if user == end:  # found the end, build the path
                 path = []
                 while user is not None:
                     path.append(user)
                     user = prev[user]
 
                 return path[::-1], depth
-            for friend in sorted(user.get_friends(), key=lambda x: x.name):
+            for friend in sorted(
+                user.get_friends(), key=lambda x: x.name
+            ):  # sort friends by name
                 if friend not in visited:
                     queue.append((friend, depth + 1))
                     visited.add(friend)
--- user.py
+++ user.py
@@ -1,13 +1,25 @@
 class User:
+    """A user in the social network."""
+
     def __init__(self, name):
+        """
+        Build a user with a name and an empty set of friends.
+        Parameters:
+            name: str
+                The user's name.
+        """
+
         self.name = name
         self.friends = set()
 
     def add_friend(self, user):
+        """Add a friend to the user's set of friends."""
         self.friends.add(user)
 
     def get_friends(self):
+        """Return the user's set of friends."""
         return self.friends
 
     def has_friend(self, user):
+        """Return True if the user is friends with the given user, otherwise return False."""
         return user in self.friends
